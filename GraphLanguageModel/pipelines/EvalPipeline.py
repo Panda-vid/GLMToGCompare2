09fc13e8e2240c09b38dddb283833f9e946c6fde
David Schwenke <schwenkedavid@t-online.de>
2024-11-27 10:05:30 +0100

    added generator unwrapping

--- GraphLanguageModel/pipelines/EvalPipeline.py
+++ GraphLanguageModel/pipelines/EvalPipeline.py
@@ -38,10 +38,11 @@ class EvalPipeline:
 
     def eval_round(self):
         scores = []
+        unwrapped_generator = self.accelerator.unwrap_model(self.generator)
         with tqdm(self.dataloader, postfix=f"Evaluating on {self.data_name}", total=len(self.dataloader), disable=(not self.accelerator.is_local_main_process)) as pbar:
             for inputs, labels in pbar:
                 with torch.no_grad():
-                    outputs = self.generator.generate(encoder_outputs=self.encoder(**inputs), max_new_tokens=self.max_generation_len)[:, :labels.shape[1]]
+                    outputs = unwrapped_generator.generate(encoder_outputs=self.encoder(**inputs), max_new_tokens=self.max_generation_len)[:, :labels.shape[1]]
                 predictions = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
                 labels = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
 
