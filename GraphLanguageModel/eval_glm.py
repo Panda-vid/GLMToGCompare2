790a531a40dbb34aa5c60eb517eca557f7e079f4
David Schwenke <schwenkedavid@t-online.de>
2025-01-27 07:59:40 +0100

    disabled gradient_checkpointing for eval

--- GraphLanguageModel/eval_glm.py
+++ GraphLanguageModel/eval_glm.py
@@ -27,7 +27,7 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
     eval_data = args.eval_file
-    model_recipe = ModelRecipe(args.encoder_modelcard, args.glm_type, args.generator_modelcard)
+    model_recipe = ModelRecipe(args.encoder_modelcard, args.glm_type, args.generator_modelcard, gradient_checkpointing=False)
     eval_pipeline_builder = EvalPipeline.Builder().is_classification_task(problem_type_to_classification_bool(args.problem_type)).set_eval_data(eval_data).add_model_recipe(model_recipe).set_batch_size(args.batch_size).set_repetitions(args.repetitions).set_device(args.device)
     eval_pipeline = eval_pipeline_builder.build()
     del eval_pipeline_builder
