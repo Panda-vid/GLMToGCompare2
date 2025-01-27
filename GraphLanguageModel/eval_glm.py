2c6979dd8b73aa875ef5d46592fe8fa8eff86a56
David Schwenke <schwenkedavid@t-online.de>
2025-01-27 10:02:22 +0100

    first part of top lever scripts

--- GraphLanguageModel/eval_glm.py
+++ GraphLanguageModel/eval_glm.py
@@ -1,15 +1,34 @@
-from pathlib import Path
+import argparse
 
+from utils.argparse import PathAction, problem_type_to_classification_bool
 from GraphLanguageModel.pipelines import EvalPipeline
 from GraphLanguageModel.pipelines.recipies import ModelRecipe
 
 
+parser = argparse.ArgumentParser(prog="eval_glm", usage="%(prog)s [options]")
+parser.add_argument("encoder_modelcard", 
+                    help="The (huggingface modelhub) location of the encoder model.", 
+                    type=str)
+parser.add_argument("generator_modelcard", 
+                    help="The (huggingface modelhub) location of the generator model.", 
+                    type=str)
+parser.add_argument("eval_file", 
+                    help="Location of the preprocessed evaluation file.", 
+                    type=str, action=PathAction)
+parser.add_argument("-pt", "--problem_type", choices=["classification", "generation"], default="classification", type=str)
+parser.add_argument("-gt", "--glm_type", help="Select whether to use a global or local GLM.", choices=["local", "global"], default="global", type=str)
+parser.add_argument("-b", "--batch_size", default=64, type=int)
+parser.add_argument("-r", "--repetitions", 
+                    help="Determines how often the evaluation should be repeated.",
+                    default=5, type=int)
+parser.add_argument("-d", "--device", default='cpu', type=str)
+args = parser.parse_args()
+
+
 if __name__ == "__main__":
-    eval_data = Path("./data/preprocessed/trex-dev-kilt.jsonl").resolve()
-    encoder_modelcard = "./saved_models/trex/flan-t5-base/encoder"
-    modelcard_generation = "./saved_models/trex/flan-t5-base/generator"
-    model_recipe = ModelRecipe(encoder_modelcard, "global", modelcard_generation)
-    eval_pipeline_builder = EvalPipeline.Builder().is_classification_task(True).set_eval_data(eval_data).add_model_recipe(model_recipe).set_device("cuda").set_batch_size(128).set_repetitions(5)
+    eval_data = args.eval_file
+    model_recipe = ModelRecipe(args.encoder_modelcard, args.glm_type, args.generator_modelcard, gradient_checkpointing=False)
+    eval_pipeline_builder = EvalPipeline.Builder().is_classification_task(problem_type_to_classification_bool(args.problem_type)).set_eval_data(eval_data).add_model_recipe(model_recipe).set_batch_size(args.batch_size).set_repetitions(args.repetitions).set_device(args.device)
     eval_pipeline = eval_pipeline_builder.build()
     del eval_pipeline_builder
     mean_score, score_std = eval_pipeline.eval()
